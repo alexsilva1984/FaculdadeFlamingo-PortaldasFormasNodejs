e0e760a8f25ff656afeed897c3161761bfc75fca
alexsilva1984 <alex.cms@hotmail.com>
2023-09-28 19:30:34 -0300

    update

---
+++ Projeto-2/index.js
@@ -0,0 +1,10 @@
+
+var x = 1.5;
+var xpto = 0 + (20 * 90 * 75 - 4 + x * ( 20 / 90 /75 + 4 + x * x) ** (20 / 90 / 75 + 4 + x * x) * (2 + 8 * (20 - 1) * - 1)* -1) + 1;
+
+
+
+console.log("Alex Silva");
+console.info("Alex Silva");
+console.error("Alex Silva");
+console.warn(xpto);
